d271ec7637c2bb884d043cc777d736990fa3cea7
Thomas Azran <tommy.azran@gmail.com>
2024-03-07 23:32:24 +0000

    mocha testing

---
+++ test/helpersTest.js
@@ -0,0 +1,18 @@
+const { assert } = require("chai");
+const { getUserByEmail } = require("../helpers");
+const { users } = ("../data");
+
+describe('getUserByEmail', function() {
+
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", users)
+    const expectedUserID = "userRandomID";
+    assert.strictEqual(user.id, expectedUserID);
+  });
+
+  it('should return undefined if a user with an invalid email is used', function() {
+    const user = getUserByEmail("invalid@example.com", users);
+    assert.isUndefined(user);
+  });
+
+});
